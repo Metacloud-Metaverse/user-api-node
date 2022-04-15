24b7fbd25f26bd8b29a6bcdae980cb0173b5affb
murtaza creensight <murtaza@creensight.com>
2022-04-15 19:03:49 +0530

    added compression to get fast API response

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 const env = require('dotenv').config()
 const express = require('express')
 const ApiRoutes = require('./routes/app.ts')
+const compression = require("compression")
 
 const port = Number(process.env.PORT || 3001)
 const app = express()
 
+app.use(compression())
 app.use(express.json())
 app.use('/api', ApiRoutes)
 
     app.listen(port, () =>
-    console.log(`Listening on ws://localhost:${port}`)
+    console.log(`Listening on http://localhost:${port}`)
 );
