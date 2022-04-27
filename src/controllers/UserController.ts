8250c1039b633d9e6189c48130feb7b86b3d36b5
murtaza creensight <murtaza@creensight.com>
2022-04-27 18:18:17 +0530

    Optimize the code and saperate functions for maximum optimization and correct the code formation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -6,15 +6,14 @@ const jwt = require("jsonwebtoken");
 const apiResponseHandler = require('../helper/ApiResponse.ts')
 
 class UserController {
-   
     static async generateGuest(req, res, next) {
         try {
-        const dateToHex = Date.now().toString(16);
-        const guest_private_secret = "AC_"+ dateToHex;
-        const data = {
-            guest_private_secret: guest_private_secret,
-        }
-            await userModel.create(data, "Guest User created successfully"); 
+            const dateToHex = Date.now().toString(16);
+            const guest_private_secret = "AC_" + dateToHex;
+            const data = {
+                guest_private_secret: guest_private_secret,
+            }
+            await userModel.create(data, "Guest User created successfully");
             let isUserExist = await UserController.userExist(guest_private_secret)
             if (!isUserExist) {
                 apiResponseHandler.sendError(req, res, "data", null, "There is some error Generating a Guest user Please try Again");
@@ -24,21 +23,20 @@ class UserController {
                     return userModel.update({ username: username }, { where: { guest_private_secret: guest_private_secret } });
                 }
                 let userUpdate = user(username)
-                userUpdate.then(async function (result){
-                console.log(result)
+                userUpdate.then(async function (result) {
+                    console.log(result)
                 })
-                const data= {
+                const data = {
                     id: isUserExist.id,
                     username: username,
                     guest_private_secret: guest_private_secret
                 }
                 apiResponseHandler.send(req, res, "data", data, "Guest-user created successfully")
             }
-     } catch(error) {
-        next(error);
+        } catch (error) {
+            next(error);
+        }
     }
-    
-}
     static async loginGuest(req, res, next) {
         try {
             //authenticate User
@@ -48,7 +46,7 @@ class UserController {
             if (!isUserExist) {
                 apiResponseHandler.sendError(req, res, "data", null, "No user exist with given user guest_privat_key");
             } else {
-                if (user_id !== isUserExist.dataValues.id){
+                if (user_id !== isUserExist.dataValues.id) {
                     apiResponseHandler.sendError(req, res, "data", null, "user_id and guest_privat_key doesn't match");
                 }
                 const accessToken = jwt.sign(
@@ -58,61 +56,55 @@ class UserController {
                         expiresIn: "8h",
                     }
                 );
-             apiResponseHandler.send(req, res, "data", { token_type: "bearer", access_token: accessToken }, "Guest Login JWT access token generated successfully")
+                apiResponseHandler.send(req, res, "data", { token_type: "bearer", access_token: accessToken }, "Guest Login JWT access token generated successfully")
             }
-            } catch (error) {
-                next(error)
-            }
+        } catch (error) {
+            next(error)
+        }
     }
     static async userList(req, res, next) {
-    try {
-        //get users form list
-        const data = req.body;
-        let userList = function () {
-            return userModel.findAll()
-        }
-        let users = userList()
-        users.then(function (result) {
-            if (Array.isArray(result) && result.length) {
-                apiResponseHandler.send(req, res, "data", result, "List all user data successfully")
-            } else {
-                apiResponseHandler.send(req, res, "data", null, "No Data found ")
+        try {
+            //get users form list
+            const data = req.body;
+            let userList = function () {
+                return userModel.findAll()
             }
-        })
+            let users = userList()
+            users.then(function (result) {
+                if (Array.isArray(result) && result.length) {
+                    apiResponseHandler.send(req, res, "data", result, "List all user data successfully")
+                } else {
+                    apiResponseHandler.send(req, res, "data", null, "No Data found ")
+                }
+            })
+        }
+        catch (error) {
+            // next(error)
+        }
     }
-    catch (error) {
-        // next(error)
-    }
-    }
-    static async saveProfile(req, res, next){
+    static async saveProfile(req, res, next) {
         try {
             //save user extra information
             const data = req.body
             if (data.email && data.username) {
-                const validateEmail = (email) => {
-                    return email.match(
-                        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                    );
-                };
-                const validateUsername = (username) => {
-                    return username.match(/^[A-Za-z][\w_]{5,14}$/);
-                };
-                if (!validateEmail(data.email)) {
+                let isValidateEmail = await UserController.validateEmail(data.email)
+                let isValidateUsername = await UserController.validateUsername(data.username)
+                if (!isValidateEmail) {
                     const message = "Email address is not valid";
                     apiResponseHandler.sendError(req, res, "data", null, message)
-                } else if (!validateUsername(data.username)) {
+                } else if (!isValidateUsername) {
                     const message = "Username is not valid, Only Uppercase [A-Z], Lowercase [a-z], Numbers[0-9], Underscore [_] are allowed and must start with letter. Length must be min 6 and max 15";
                     apiResponseHandler.sendError(req, res, "data", null, message)
                 } else {
                     let isUsernameExist = await UserController.usernameExist(data.username, req.user.user_id)
                     let isEmailExist = await UserController.emailExist(data.email, req.user.user_id)
-                    if(isUsernameExist){
+                    if (isUsernameExist) {
                         apiResponseHandler.sendError(req, res, "data", null, "This username is alredy taken. Please try again with some other username");
-                    }else if (isEmailExist) {
+                    } else if (isEmailExist) {
                         apiResponseHandler.sendError(req, res, "data", null, "This email is already registered");
                     } else {
-                    await userModel.update(data, { where: { id: req.user.user_id } });
-                    apiResponseHandler.send(req, res, "data", data, "User Profile saved successfully")
+                        await userModel.update(data, { where: { id: req.user.user_id } });
+                        apiResponseHandler.send(req, res, "data", data, "User Profile saved successfully")
                     }
                 }
             }
@@ -120,14 +112,22 @@ class UserController {
             apiResponseHandler.sendError(req, res, "data", null, "Error saving user profile. Please try again with correct data.");
         }
     }
+    static async validateEmail(email) {
+        return email.match(
+            /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        );
+    }
+    static async validateUsername(username) {
+        return username.match(/^[A-Za-z][\w_]{5,14}$/);
+    }
     static async userExist(guest_private_secret) {
         return userModel.findOne({ where: { guest_private_secret: guest_private_secret } })
     }
     static async usernameExist(username, id) {
-        return userModel.findOne({ where: {[Op.and]: [ { username: username }, { id: { [Op.not]: id} }] }})
+        return userModel.findOne({ where: { [Op.and]: [{ username: username }, { id: { [Op.not]: id } }] } })
     }
     static async emailExist(email, id) {
-        return userModel.findOne({ where: { [Op.and]: [{ email: email },  { id: { [Op.not]: id} }] } })
+        return userModel.findOne({ where: { [Op.and]: [{ email: email }, { id: { [Op.not]: id } }] } })
     }
 }
 module.exports = UserController;
