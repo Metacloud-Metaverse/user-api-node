e6242e98e3d52a64bc9b6d03be7ef25593527ed4
murtaza creensight <murtaza@creensight.com>
2022-05-17 20:32:40 +0530

    validation for user setting save API fields data types

--- src/controllers/UserSettingController.ts
+++ src/controllers/UserSettingController.ts
@@ -1,8 +1,5 @@
 const dbs = require('../models/index.js');
 const userSettingModel = dbs.user_setting;
-const Sequelize = require('sequelize');
-const Op = Sequelize.Op;
-const jwt = require("jsonwebtoken");
 const apiResponseHandler = require('../helper/ApiResponse.ts')
 
 class UserSettingController {
@@ -11,8 +8,10 @@ class UserSettingController {
       const data = req.body
       data.user_id = req.user.user_id
       console.log(data)
-      await userSettingModel.create(data)
-      apiResponseHandler.send(req, res, "data", data, "User settings saved successfully")
+      if (await UserSettingController.checkValidation(req, res, data)) {
+        await userSettingModel.create(data)
+        apiResponseHandler.send(req, res, "data", data, "User settings saved successfully")
+      }
     } catch {
       apiResponseHandler.sendError(req, res, "data", null, "Error saving user settings. Please try again with correct data.");
     }
@@ -25,6 +24,26 @@ class UserSettingController {
     } catch (error) {
       const message = "Error fetching product, Please try again with correct data"
       apiResponseHandler.sendError(req, res, "data", null, message)
+    }
+  }
+  static async checkValidation(req, res, data) {
+    if (data.player_name_opacity  === null || data.player_name_opacity  === "" || (data.player_name_opacity  && (data.player_name_opacity  > 1 || isNaN(data.player_name_opacity )))) {
+      const message = "player_name_opacity value is not valid, Value should be between 0 to 1"
+      apiResponseHandler.sendError(req, res, "data", null, message)
+    } else if (data.scene_load_radius === null || data.scene_load_radius === "" || (data.scene_load_radius && (data.scene_load_radius > 1 || isNaN(data.scene_load_radius)))) {
+      const message = "scene_load_radius value is not valid, Value should be between 0 to 1"
+      apiResponseHandler.sendError(req, res, "data", null, message)
+    } else if (data.rendering_scale === null || data.rendering_scale === "" || (data.rendering_scale && (data.rendering_scale > 1 || isNaN(data.rendering_scale)))) {
+      const message = "rendering_scale value is not valid, Value should be between 0 to 4"
+      apiResponseHandler.sendError(req, res, "data", null, message)
+    } else if (data.volume_master === null || data.volume_master === "" || (data.volume_master && (data.volume_master > 1 || isNaN(data.volume_master)))) {
+      const message = "volume_master value is not valid, Value should be between 0 to 1"
+      apiResponseHandler.sendError(req, res, "data", null, message)
+    } else if (data.volume_music === null || data.volume_music === "" || (data.volume_music && (data.volume_music > 1 || isNaN(data.volume_music)))) {
+      const message = "volume_music value is not valid, Value should be between 0 to 1"
+      apiResponseHandler.sendError(req, res, "data", null, message)
+    }else{
+      return true
     }
   }
 }
