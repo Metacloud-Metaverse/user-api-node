7991768f3bb287d6650f87ec4acd2588832f488e
murtaza creensight <murtaza@creensight.com>
2022-06-07 19:33:51 +0530

    install cors in npm and use in app

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ const env = require('dotenv').config()
 const express = require('express')
 const ApiRoutes = require('./routes/app.ts')
 const compression = require("compression")
+const cors = require('cors')
 
 const port = Number(process.env.PORT || 3001)
 const app = express()
 
+app.use(cors())
 app.use(compression())
 app.use(express.json())
 app.use('/', ApiRoutes)
